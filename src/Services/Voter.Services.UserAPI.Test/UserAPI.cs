08d00e67d8b8464165f9040cdc9f3d7885e704b4
Yasassri Purnajith <purnajith@outlook.com>
2019-07-01 09:31:17 +0530

    Fixes

---
+++ src/Services/Voter.Services.UserAPI.Test/UserAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Voter.Services.UserAPI.Controllers;
+using Xunit;
+
+namespace Voter.Services.UserAPI.Test
+{
+	public class UserAPI
+	{
+		[Fact]	
+		public void TestAPICall()
+		{
+			var controller = new UnitTestController();
+			IEnumerable<string> result = controller.Get();
+
+			Assert.Equal(2, result.Count());
+
+		}
+	}
+}
